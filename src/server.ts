e4c31031df1d7ec190532a11a57393c43dba64f1
Victor Araújo <48557502+Victorb999@users.noreply.github.com>
2021-02-14 11:03:54 -0300

    add cors

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,16 @@
 import express from 'express'
 import routes from './routes'
+var cors = require('cors')
 
 const app = express()
+
+var corsOptions = {
+  "origin": "*",
+  "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+}
+
+app.use(cors(corsOptions))
+app.use(express.static('public'))
 app.use(express.json())
 app.use(routes)
 
