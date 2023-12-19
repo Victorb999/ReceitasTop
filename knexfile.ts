cb65fa9129880c4e38be8abd46ed6df668a4fb07
Victor Araújo <victor.b_@hotmail.com>
2023-12-19 19:46:42 +0000

    refactor: Update database connection configuration
    Update database connection configuration to use environment variable

--- knexfile.ts
+++ knexfile.ts
@@ -1,9 +1,9 @@
 import path from "path";
+import "dotenv/config";
 
 module.exports = {
   client: "pg",
-  connection:
-    "postgresql://Victorb999:lckJFjIhQ6z3@ep-calm-haze-67502538.us-east-2.aws.neon.tech/receitaTop?sslmode=require",
+  connection: process.env.NODE_DB_CONNECTION,
   searchPath: ["knex", "public"],
   migrations: {
     directory: path.resolve(__dirname, "src", "database", "migrations"),
--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,10 +1,9 @@
 import knex from "knex";
-import path from "path";
+import "dotenv/config";
 
 const connection = knex({
   client: "pg",
-  connection:
-    "postgresql://Victorb999:lckJFjIhQ6z3@ep-calm-haze-67502538.us-east-2.aws.neon.tech/receitaTop?sslmode=require",
+  connection: process.env.NODE_DB_CONNECTION,
   searchPath: ["knex", "public"],
   useNullAsDefault: true,
 });
