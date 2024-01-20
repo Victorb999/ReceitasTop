e8f7ef5e802a2392eb90db74cd7fc9f419081558
victorb999 <victor.b_@hotmail.com>
2024-01-20 17:12:11 -0300

    add host

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,20 @@
-import express from 'express'
-import routes from './routes'
-var cors = require('cors')
+import express from "express";
+import routes from "./routes";
+var cors = require("cors");
 
-const app = express()
-require('dotenv').config()
+const app = express();
+require("dotenv").config();
 
 var corsOptions = {
-  "origin": "*",
-  "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
-}
+  origin: "*",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+};
 
-app.use(cors(corsOptions))
-app.use(express.static('public'))
-app.use(express.json())
-app.use(routes)
-app.listen(process.env.PORT || 3333)
+app.use(cors(corsOptions));
+app.use(express.static("public"));
+app.use(express.json());
+app.use(routes);
+app.listen({
+  host: "0.0.0.0",
+  port: process.env.PORT || 3333,
+});
